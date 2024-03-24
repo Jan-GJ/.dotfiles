da05db8c479af8d921d6e952a2c00d155e1d3dd0
Jan Große Jüttermann <gameforsz@gmail.com>
2024-03-24 20:03:54 +0100

    feat: added git clone statements for pm's and plugins

--- update.sh
+++ update.sh
@@ -4,9 +4,21 @@ cp .clang-format ~/.clang-format
 cp .zshrc ~/.zshrc
 cp .p10k.zsh ~/.p10k.zsh
 
-if [ -d ~/.config ]; then
-	mdkir ~/.config
-	echo "Created .config"
-fi
+cp -r .config/ ~/.config
 
-cp -r .config ~/.config
+#zsh
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/z-shell/F-Sy-H.git \
+  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/F-Sy-H
+
+#nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+#tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+tmux source ~/.config/tmux/tmux.conf
